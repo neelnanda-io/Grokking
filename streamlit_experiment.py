5c990ae3c2b4d1978cd05bb6cac9434dc7567956
Neel Nanda <neelnanda27@gmail.com>
2022-09-25 04:21:24 -0700

    Streamlit test

---
+++ streamlit_experiment.py
@@ -0,0 +1,12 @@
+# %%
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default = 'vscode'
+import numpy as np
+
+import streamlit as sd
+# %%
+fig = px.line(np.arange(5))
+# fig.show()
+sd.plotly_chart(fig)
+# %%
